2c35e97ac0c748efee8a1324172f6a6e14b04733
tubyliec <antikruk@vivaldi.net>
2025-10-26 11:04:21 +0300

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+    const args = process.argv.slice(2);
+    const values = args.reduce((prevValue, curValue, curIndex, array) => {
+        let result;
+        if (curIndex % 2 === 0) {
+            result = `${curValue.slice(2)} is `;
+        } else {
+            result = curValue;
+            if (curIndex !== array.at(-1)) {
+                result = `${result}, `;
+            }
+        }
+        return prevValue + result;
+    }, "");
+    console.log(values);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-  // Write your code here
+    const filteredVariables = Object.entries(process.env).filter(([key]) => key.includes("RSS_"));
+    for (let variable of filteredVariables) {
+        console.log(`RSS_name${filteredVariables.indexOf(variable) + 1}=${variable[1]}`);
+    }
 };
 
 parseEnv();
