8c7fdc53b1e1f7841d12bceeffa0468566ebac31
tubyliec <antikruk@vivaldi.net>
2025-10-26 11:37:25 +0300

    feat: add cp solutions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+    const childProcess = spawn('node', ['src/cp/files/script.js', ...args]);
+
+    process.stdin.on('data', (data) => {
+        childProcess.stdin.write(data);
+    });
+
+    childProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    childProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    childProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        process.exit(0);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [1, 2]);
