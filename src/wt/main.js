25a45ee122f0b580b85793dc5c4cf0a8847c5858
tubyliec <antikruk@vivaldi.net>
2025-10-26 22:49:10 +0300

    feat: add wt and zip solutions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,19 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { join } from 'path';
+
 const performCalculations = async () => {
-  // Write your code here
+    const dirName = import.meta.dirname;
+    const results = await Promise.all(
+        cpus().map((_, index) => new Promise((resolve) => {
+            const worker = new Worker(join(dirName, 'worker.js'), {
+                workerData: 10 + index
+            });
+            worker.on('message', resolve);
+            worker.on('error', () => resolve({ status: 'error', data: null }));
+        }))
+    );
+    console.log(results);
 };
 
 await performCalculations();
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { fileURLToPath  } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createWriteStream, createReadStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+const fileName = fileURLToPath(import.meta.url);
+
 const compress = async () => {
-  // Write your code here
+    const filePath = join(dirname(fileName), 'files/fileToCompress.txt');
+    const streamRead = createReadStream(filePath);
+    const archivePath = join(dirname(fileName), 'files/archive.gz');
+    const streamWrite = createWriteStream(archivePath);
+    const gzip = createGzip();
+    await pipeline(streamRead, gzip, streamWrite);
 };
 
 await compress();
