4fef33a97218696cb91bb39e4318ac4328b46340
tubyliec <antikruk@vivaldi.net>
2025-10-26 21:47:59 +0300

    feat: add streams and modules solutions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { fileURLToPath  } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createReadStream } from 'node:fs';
+
+const fileName = fileURLToPath(import.meta.url);
+
 const read = async () => {
-  // Write your code here
+    const path = join(dirname(fileName), 'files/fileToRead.txt');
+    const stream = createReadStream(path, { encoding: 'utf8' });
+
+    stream.pipe(process.stdout);
+    stream.on('end', () => {
+        process.stdout.write('\n');
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { fileURLToPath  } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createWriteStream } from 'node:fs';
+
+const fileName = fileURLToPath(import.meta.url);
+
 const write = async () => {
-  // Write your code here
+    const path = join(dirname(fileName), 'files/fileToWrite.txt');
+    const stream = createWriteStream(path, { encoding: 'utf8' });
+
+    process.stdin.pipe(stream);
 };
 
 await write();
